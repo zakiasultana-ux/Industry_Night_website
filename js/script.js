8c3898a4562ff2c14a63a1a8d8862219e732eba0
zakiasultana-ux <z_zakiasultana@fanshaweonline.ca>
2025-09-23 18:43:36 -0400

    new code
    Created a new HTML class. Updated the CSS and wrote JS code for the mobile screen menu

--- js/script.js
+++ js/script.js
@@ -2,10 +2,21 @@ console.log("welcome to the Industry Night website!");
 
 // variables
 
-
+const menu = document.querySelector(".menu");
+const menuItems = document.querySelectorAll(".menuItem");
+const hamburger= document.querySelector(".hamburger");
 
 // functions 
 
-
+function togglemenu() {
+    console.log("toggle menu"); 
+    if (menu.classList.contains("showMenu")) {
+    menu.classList.remove("showMenu");
+  } else {
+    menu.classList.add("showMenu");
+  }
+}
 
 // event listeners
+
+hamburger.addEventListener('click', togglemenu);
